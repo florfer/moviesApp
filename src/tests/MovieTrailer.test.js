317e4839260f5d763bc9747a6d83f4e1e9550497
Florencia Fernandez <florenciafermarti@gmail.com>
2019-10-06 11:45:41 -0300

    Unit test fixed

--- src/tests/MovieTrailer.test.js
+++ src/tests/MovieTrailer.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './../store';
 import MovieTrailer from './../components/MovieTrailer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const trailer = [];
-  ReactDOM.render(<MovieTrailer trailer = { trailer } />, div);
+  const match = {
+    params: {
+        id: 456456,
+    }
+  };
+  /* const trailer = []; */
+  ReactDOM.render(<Provider store={store}><MovieTrailer match={ match } /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
